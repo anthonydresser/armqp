3872812ca3af6a49a23cdf71dfc6c7a4006e652d
Anthony Dresser <smiley.yellow@gmail.com>
2015-09-07 14:03:27 -0400

    testing adding files

---
+++ src/hdl/main.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/07/2015 02:02:54 PM
+// Design Name: 
+// Module Name: main
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module main(
+
+    // some content
+    );
+endmodule
