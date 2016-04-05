662ba93f4a7ba51cc53735bb9253ab6b0f24f415
Andrew Weiler <thesmallterror+github@gmail.com>
2016-04-04 23:39:34 -0400

    Fixing unsythesizable block diagram. Rebuilt block design from Jan 24 commit

--- armqp_math_tester/armqp_math_tester.sim/sim_1/behav/simulate.bat
+++ armqp_math_tester/armqp_math_tester.sim/sim_1/behav/simulate.bat
@@ -1,6 +1,6 @@
 @echo off
 set xv_path=C:\\XilinxVivado\\Vivado\\2015.2\\bin
-call %xv_path%/xsim math_tester_behav -key {Behavioral:sim_1:Functional:math_tester} -tclbatch math_tester.tcl -log simulate.log
+call %xv_path%/xsim math_tester_behav -key {Behavioral:sim_1:Functional:math_tester} -tclbatch math_tester.tcl -view D:/vivadoprojects/armqp/src/simulation_assets/math_tester_behav.wcfg -log simulate.log
 if "%errorlevel%"=="0" goto SUCCESS
 if "%errorlevel%"=="1" goto END
 :END
