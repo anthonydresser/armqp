6e81956fb26ec64908603586913e815b8afb616c
lhunker <lukas.hunker@gmail.com>
2016-04-26 21:15:09 -0400

    Removing extraneous files, round 1

--- armqp_math_tester/armqp_math_tester.sim/sim_1/behav/elaborate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\XilinxVivado\\Vivado\\2015.2\\bin
-call %xv_path%/xelab  -wto 5aa0a778f6a840c080f5e8d83b527d5b -m64 --debug typical --relax --mt 2 -L xil_defaultlib -L unisims_ver -L unimacro_ver -L secureip --snapshot math_tester_behav xil_defaultlib.math_tester xil_defaultlib.glbl -log elaborate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
--- armqp_math_tester/armqp_math_tester.sim/sim_1/behav/simulate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\XilinxVivado\\Vivado\\2015.2\\bin
-call %xv_path%/xsim math_tester_behav -key {Behavioral:sim_1:Functional:math_tester} -tclbatch math_tester.tcl -view D:/vivadoprojects/armqp/src/simulation_assets/math_tester_behav.wcfg -log simulate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
